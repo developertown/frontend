529ab39a7739d15679e6831fb3c340b60044ebc0
Alex Billingsley <abillingsley@developertown.com>
2020-05-01 10:30:42 -0400

    update react error Boundary dependency

--- react/cra-template/template/src/ui/index.tsx
+++ react/cra-template/template/src/ui/index.tsx
@@ -1,12 +1,18 @@
 import React from "react";
-import ErrorBoundary from "react-error-boundary";
+import { ErrorBoundary, FallbackProps } from "react-error-boundary";
 import { Redirect, Route, Switch } from "react-router";
 import { BrowserRouter as Router } from "react-router-dom";
 import Example from "../components/app";
 import { routes } from "./routes";
 
-const UnhandledError: React.FC = () => {
-  return <div>Oops...</div>;
+const UnhandledError: React.FC<FallbackProps> = ({ error, componentStack }) => {
+  return (
+    <div role="alert">
+      <p>Something went wrong:</p>
+      <pre>{error?.message}</pre>
+      <pre>{componentStack}</pre>
+    </div>
+  );
 };
 
 const App: React.FC = () => {
