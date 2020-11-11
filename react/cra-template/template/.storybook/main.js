8750e4ababa2b90333382c5154b6d84f3d55bd21
Jason Vasquez <jasonvasquez@users.noreply.github.com>
2020-11-11 10:57:12 -0500

    Updating glob pattern for storybook 6.x plus, and bumping rev (#22)

--- react/cra-template/template/.storybook/main.js
+++ react/cra-template/template/.storybook/main.js
@@ -1,5 +1,5 @@
 module.exports = {
-  stories: ["../src/**/*.(stories|story).(js|jsx|ts|tsx)"],
+  stories: ["../src/**/*.@(stories|story).@(js|jsx|ts|tsx)"],
   addons: [
     "@storybook/preset-create-react-app",
     "@storybook/addon-essentials",
