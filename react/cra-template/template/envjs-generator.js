8b22089af18587952857de0b1222c7a104f55eeb
Alex Billingsley <abillingsley@developertown.com>
2021-03-21 07:03:09 -0400

    adds formating scripts and build steps

--- react/cra-template/template/envjs-generator.js
+++ react/cra-template/template/envjs-generator.js
@@ -2,11 +2,12 @@ const fs = require("fs");
 const args = process.argv.slice(2);
 
 const outputFile = args[0];
-const varObject = Object
-  .keys(process.env)
-  .reduce((vars, key) => ({
+const varObject = Object.keys(process.env).reduce(
+  (vars, key) => ({
     ...vars,
-    [key]: key.startsWith("REACT_APP_") ? process.env[key] : undefined
-  }), {REACT_APP_VERSION: new Date().getTime()});
+    [key]: key.startsWith("REACT_APP_") ? process.env[key] : undefined,
+  }),
+  { REACT_APP_VERSION: new Date().getTime() },
+);
 
 fs.writeFileSync(outputFile, `window.env = ${JSON.stringify(varObject, null, 2)}`);
